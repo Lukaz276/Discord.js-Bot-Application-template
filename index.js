7910f2692c1e9ed4da409b57848855c1dc51b8e9
Lukaz276 <48632706+Lukaz276@users.noreply.github.com>
2022-01-25 19:16:53 +0100

    Update deploy_commands.js
    Update deploy_guild_commands.js
    
    Initial Commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const { Client, Intents } = require('discord.js');
+const fs = require('fs');
+
+const { token } = require('./config.json');
+
+
+const client = new Client({intents: [Intents.FLAGS.GUILDS]});
+
+
+const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
+for (const file of eventFiles) {
+	const event = require(`./events/${file}`);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args));
+	} else {
+		client.on(event.name, (...args) => event.execute(...args));
+	}
+}
+
+
+
+client.login(token);
