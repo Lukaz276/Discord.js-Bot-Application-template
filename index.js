84ee3a19979d81fed588632440d68e48e92d2a90
Lukaz276 <48632706+Lukaz276@users.noreply.github.com>
2022-01-27 16:02:35 +0100

    Updated command handling

--- index.js
+++ index.js
@@ -2,11 +2,44 @@ const { Client, Intents } = require('discord.js');
 const fs = require('fs');
 
 const { token } = require('./config.json');
-
-
 const client = new Client({intents: [Intents.FLAGS.GUILDS]});
 
 
+/**
+ * Command handling for all files in ./commands
+ */
+
+client.commands = new Collection();
+const commandFiles = fs.readdirSync('./commands').filter((file) => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	// @ts-ignore
+	client.commands.set(command.data.name, command);
+}
+
+
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;
+
+	const command = client.commands.get(interaction.commandName);
+
+	if (!command) return;
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+	}
+});
+
+
+
+
+/**
+ * Event Handling of all files in ./events
+ */
 const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
 for (const file of eventFiles) {
 	const event = require(`./events/${file}`);
