b93915934def9dd54d8044c23130caee2f058456
Lukaz276 <48632706+Lukaz276@users.noreply.github.com>
2022-01-27 16:31:50 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { Client, Intents } = require('discord.js');
+const { Client, Intents, Collection } = require('discord.js');
 const fs = require('fs');
 
 const { token } = require('./config.json');
